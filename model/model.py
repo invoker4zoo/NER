09d058725a5f57f98f74b8b80815b56e5864647e
cc <412214410@qq.com>
2018-09-30 17:58:31 +0800

    update model

--- model/model.py
+++ model/model.py
@@ -70,6 +70,7 @@ class BiLSTM_CRF(object):
 
         # build bi-lstm layer op
         with tf.variable_scope('bi-lstm'):
+            # self.hidden_dim the unit nums in lstm cell
             cell_fw = LSTMCell(self.hidden_dim)
             cell_bw = LSTMCell(self.hidden_dim)
             (output_fw_seq, output_bw_seq), _ = tf.nn.bidirectional_dynamic_rnn(
@@ -80,3 +81,39 @@ class BiLSTM_CRF(object):
                 dtype=tf.float32)
             output = tf.concat([output_fw_seq, output_bw_seq], axis=-1)
             output = tf.nn.dropout(output, self.dropout_pl)
+
+        with tf.variable_scope('layer_out'):
+            """
+            Returns an initializer performing "Xavier" initialization for weights.
+            This function implements the weight initialization from:
+            Xavier Glorot and Yoshua Bengio (2010): Understanding the difficulty of training \
+            deep feedforward neural networks. International conference on artificial intelligence \
+            and statistics.
+
+            This initializer is designed to keep the scale of the gradients roughly the same \
+            in all layers. In uniform distribution this ends up being the range: x = sqrt(6. / (in + out)); \
+            [-x, x] and for normal distribution a standard deviation of sqrt(2. / (in + out)) is used.
+            """
+            W = tf.get_variable(name='W', shape=[2 * self.hidden_dim, self.num_tags],\
+                                initializer=tf.contrib.layers.xavier_initializer(),\
+                                dtype=tf.float32)
+
+            B = tf.get_variable(name='B', shape=self.num_tags, initializer=tf.zeros_initializer(),\
+                                dtype=tf.float32)
+
+            # out_shape[-1]-> step
+            out_shape = tf.shape(output)
+            output = tf.reshape(output, [-1, 2 * self.hidden_dim])
+            self.logits = tf.reshape((tf.matmul(output, W) + B), [-1, out_shape[1], self.num_tags])
+
+        # build loss op
+        if self.CRF:
+            # CRF out layer
+            with tf.variable_scope('crf_loss'):
+                log_likelihood, self.trans = crf_log_likelihood(inputs=self.logits,tag_indices=self.labels,\
+                                                                sequence_lengths=self.sequence_lengths)
+                self.loss = -tf.reduce_mean(log_likelihood)
+        else:
+            # with softmax out layer
+            with tf.variable_scope('softmax_loss'):
+
